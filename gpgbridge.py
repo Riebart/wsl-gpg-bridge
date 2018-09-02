1959542f9e7e1f415a862eedec60a82cbfc30b03
Mike Himbeault <mike@riebart.ca>
2018-09-02 15:22:30 -0500

    A couple mior changes + line-endings

--- gpgbridge.py
+++ gpgbridge.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import socket
 import select
@@ -8,12 +10,15 @@ unix_socket = sys.argv[2]
 
 
 def handle(sock, address, remote_address, preamble):
+    # Reference:
+    # - https://dev.gnupg.org/T3883
     rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     rs.connect(remote_address)
+    print("Sending connection nonce")
     rs.sendall(preamble)
     while True:
-        irdy, ordy, erdy = select.select([rs, sock], [], [], 0.1)
-        if rs in irdy:
+        input_ready, _, _ = select.select([rs, sock], [], [], 0.1)
+        if rs in input_ready:
             print("Remote has stuff")
             buf = rs.recv(4096)
             # If we've been notified there's a receive, but no bytes read, then
@@ -23,7 +28,7 @@ def handle(sock, address, remote_address, preamble):
             else:
                 print("To unix:", buf)
                 sock.sendall(buf)
-        if sock in irdy:
+        if sock in input_ready:
             print("Unix has stuff")
             buf = sock.recv(4096)
             if len(buf) == 0:
@@ -42,14 +47,14 @@ with open(assuan_socket, "rb") as fp:
     windows_port = int(fp.readline().strip().decode("ascii"))
     windows_payload = fp.read()
 
-us = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+us = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)  # pylint: disable=E1101
 us.bind(unix_socket)
 us.listen(1)
 
 while True:
     print("Selecting...")
-    irdy, ordy, erdy = select.select([us], [], [], 5)
-    if len(irdy) > 0:
+    input_ready, _, _ = select.select([us], [], [], 5)
+    if len(input_ready) > 0:
         # handle the server socket
         try:
             client, address = us.accept()
